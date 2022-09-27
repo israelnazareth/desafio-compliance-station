29fc7c30bc82d2bd259ce15507d2dd4aed7be699
Israel Nazareth <israel.fab@hotmail.com>
2022-09-27 19:55:54 -0300

    feat: adiciona os ícones à sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,24 @@
+import { FiMenu, FiGlobe, FiHeadphones, FiSettings } from 'react-icons/fi';
+import { AiOutlineFolder } from 'react-icons/ai'
+import { MdOutlinePeopleAlt } from 'react-icons/md'
+import { GrAnalytics, GrDocumentText } from 'react-icons/gr'
 import './Sidebar.css'
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-
+      <FiMenu className='hamburger'/>
+      <p className='area'>STATION</p>
+      <FiGlobe className='icons'/>
+      <AiOutlineFolder className='icons'/>
+      <hr />
+      <p className='area'>FORMS</p>
+      <MdOutlinePeopleAlt className='icons'/>
+      <GrDocumentText className='icons'/>
+      <GrAnalytics className='icons'/>
+      <hr />
+      <FiSettings className='icons'/>
+      <FiHeadphones className='icons'/>
     </div>
   )
 }
