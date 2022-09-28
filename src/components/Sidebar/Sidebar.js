6e446e7337d9933928a82066c0ecdaeaaf69323a
Israel Nazareth <israel.fab@hotmail.com>
2022-09-27 23:54:46 -0300

    feat: adiciona lógica de seleção dos ícones

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { FiMenu, FiGlobe, FiHeadphones, FiSettings } from 'react-icons/fi';
 import { AiOutlineFolder } from 'react-icons/ai'
 import { MdOutlinePeopleAlt } from 'react-icons/md'
@@ -5,20 +6,64 @@ import { GrAnalytics, GrDocumentText } from 'react-icons/gr'
 import './Sidebar.css'
 
 const Sidebar = () => {
+  const [items, setItems] = useState([])
+
+  const changeColor = () => {
+    const icons = document.getElementsByName('icons2');
+    icons.forEach((item) => {
+      const attr = item.innerHTML;
+      const newLine = attr.replace(' stroke="#000"', '');
+      item.innerHTML = newLine
+    })
+  }
+
+  const selectedIcon = (e) => {
+    items.forEach((item) => {
+      if (e.target.name === 'buttons') {
+        e.target.classList.add('selected')
+      }
+      if (item.classList.contains('selected')) {
+        item.classList.remove('selected')
+      }
+      console.log(item.classList)
+      console.log(items);
+    })
+  }
+
+  useEffect(() => {
+    changeColor();
+    const buttons = document.querySelectorAll('button')
+    setItems(buttons)
+  }, [])
+
   return (
     <div className="sidebar">
       <FiMenu className='hamburger'/>
       <p className='area'>STATION</p>
-      <FiGlobe className='icons'/>
-      <AiOutlineFolder className='icons'/>
+      <button name='buttons' className='buttonIcons' onClick={(e) => selectedIcon(e)}>
+        <FiGlobe className='icons' />
+      </button>
+      <button name='buttons' className='buttonIcons' onClick={(e) => selectedIcon(e)}>
+        <AiOutlineFolder className='icons' />
+      </button>
       <hr />
       <p className='area'>FORMS</p>
-      <MdOutlinePeopleAlt className='icons'/>
-      <GrDocumentText className='icons'/>
-      <GrAnalytics className='icons'/>
+      <button name='buttons' className='buttonIcons selected' onClick={(e) => selectedIcon(e)}>
+        <MdOutlinePeopleAlt className='icons' />
+      </button>
+      <button name='buttons' className='buttonIcons' onClick={(e) => selectedIcon(e)}>
+        <GrDocumentText className='icons' name='icons2' />
+      </button>
+      <button name='buttons' className='buttonIcons' onClick={(e) => selectedIcon(e)}>
+        <GrAnalytics className='icons' name='icons2' />
+      </button>
       <hr />
-      <FiSettings className='icons'/>
-      <FiHeadphones className='icons'/>
+      <button name='buttons' className='buttonIcons' onClick={(e) => selectedIcon(e)}>
+        <FiSettings className='icons' />
+      </button>
+      <button name='buttons' className='buttonIcons' onClick={(e) => selectedIcon(e)}>
+        <FiHeadphones className='icons' />
+      </button>
     </div>
   )
 }
